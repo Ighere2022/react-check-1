a13dd07086867e9a321f29337be40e508fc7dd0e
Ighere2022 <Uyoyo2003@gmail>
2024-06-11 12:12:30 +0100

    second commit

--- player-card/src/App.jsx
+++ player-card/src/App.jsx
@@ -6,10 +6,18 @@ import { Boots } from "./Product";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 function App() {
+  const [userName, setUserName] = useState("");
+
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        onChange={(e) => setUserName(e.target.value)}
+      />
       <Card style={{ width: "18rem" }}>
         <Image image={Boots.Image} />
         <Card.Body>
@@ -19,6 +27,14 @@ function App() {
           <Button variant="primary">Buy Now</Button>
         </Card.Body>
       </Card>
+      <p>Hello {userName === "" ? "there!" : userName}</p>
+      {userName !== "" && (
+        <img
+          src="https://i.pinimg.com/736x/15/35/4c/15354ca8b8cf3712622f2de21997b96c.jpg"
+          alt="hdfdfd"
+          style={{ width: "150px" }}
+        />
+      )}
     </div>
   );
 }
